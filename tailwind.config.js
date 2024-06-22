60ba003b900853f1d1d0e14378dc2ee0e0241c33
ROBERT9 <robertocarlos.perez141@gmail.com>
2024-06-22 00:11:13 -0500

    App Vaca

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./www/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'fc1': '#FFFFFF',
+        'fc2': '#4169E1',
+        'fc3': '#D2DCF8',
+        'fc4': '#7B7878',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
